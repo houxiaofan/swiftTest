68eab203d457d756d087ff2351d7de78f84812be
houxiaofan <46988025@qq.com>
2018-01-23 10:59:32 +0800

    test

--- swiftTest/ViewController.swift
+++ swiftTest/ViewController.swift
@@ -7,13 +7,46 @@
 //
 
 import UIKit
+//import Cocoa
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+//        var myString : String!
+//        myString = "Hello,Swift!"
+//        if myString != nil {
+//            print(myString)
+//        }else{
+//            print("myString 值为 nil")
+//        }
+        
+        var myString:String?
+        myString = "Hello,Swift!"
+        if let yourString = myString {
+            print("你的字符串值为-\(yourString)")
+        }else{
+            print("你的字符串没有值")
+        }
+        
+        let someDict:[Int:String] = [1:"One",2:"Two",3:"Three"]
+        for (key, value) in someDict {
+            print("字典key\(key)-字典value\(value)")
+        }
+        
+        print(runoob(site: "shaziyo"))
+        
     }
+    @IBAction func btn(_ sender: Any) {
+        print("This is a Git tutorial")
+    }
+    
+    func runoob(site:String) -> String {
+        return (site)
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
